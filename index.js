726855625250c4403088b8e5f643524a90659bb1
Narasimha Uppala <sainarasimha8374216883@gmail.com>
2024-08-12 00:12:59 +0530

    Update index.js
    updated port

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ app.post('/api/banner', (req, res) => {
   );
 });
 
-const PORT = process.envPORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
